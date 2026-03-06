c4f060be03c33daa5460beee3bdf841247e52954
Shobha Ch <151423828+cherupallyshobha@users.noreply.github.com>
2026-03-06 17:00:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import RandomNumberGenerator from './Component/RandomNumberGenerator'
-import './index.css' 
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
-const App = () => {
-  return(
-    <div className="container">
-        <RandomNumberGenerator/>
-    </div>            
-  )
-}
+import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart' 
+import Products from './components/Products' 
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+
+const App = () => (
+                    
+  <BrowserRouter>
+   <Switch>
+    <Route exact path = "/login" component = {LoginForm}/>
+    <ProtectedRoute exact path = "/" component = {Home}/>
+    <ProtectedRoute exact path = "/cart" component = {Cart}/>
+    <ProtectedRoute exact path = "/products" component = {Products}/>
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+   </Switch>
+  </BrowserRouter>
+
+)
 export default App
